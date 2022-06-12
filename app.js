5a634dd05980dd13e2d451b8158d97286672aeb5
Julian <julianjesacher@gmail.com>
2022-06-12 13:30:24 +0200

    Set darkmode according to system or chosen preference

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-document.getElementById("darkmode-checkbox").addEventListener("click", (ev) => {
+//Darkmode
+
+document.getElementById("darkmode-checkbox").addEventListener("click", (event) => {
     document.body.classList.toggle("darkmode");
+    localStorage.setItem("darkmodeActive", event.target.checked);
+    console.log(event.target.checked);
 });
+
+function setDarkModePreference() {
+    const localStorageSetting = localStorage.getItem("darkmodeActive");
+    if (!localStorageSetting && window.matchMedia("(prefers-color-scheme:dark)").matches) {
+        document.body.classList.add("darkmode");
+        document.getElementById("darkmode-checkbox").checked = true;
+    }
+
+    if (localStorageSetting === "true") {
+        document.body.classList.add("darkmode");
+        document.getElementById("darkmode-checkbox").checked = true;
+    }
+}
+window.onload = setDarkModePreference;
