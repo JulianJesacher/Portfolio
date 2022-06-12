15b9991948d44e01792fbe64ad40928d48e94995
Julian <julianjesacher@gmail.com>
2022-06-12 23:10:06 +0200

    Switch text color with darkmode

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //Darkmode
-
 document.getElementById("darkmode-checkbox").addEventListener("click", (event) => {
     document.body.classList.toggle("darkmode");
     localStorage.setItem("darkmodeActive", event.target.checked);
@@ -8,14 +7,19 @@ document.getElementById("darkmode-checkbox").addEventListener("click", (event) =
 
 function setDarkModePreference() {
     const localStorageSetting = localStorage.getItem("darkmodeActive");
+    const checkBox = document.getElementById("darkmode-checkbox");
+
     if (!localStorageSetting && window.matchMedia("(prefers-color-scheme:dark)").matches) {
         document.body.classList.add("darkmode");
-        document.getElementById("darkmode-checkbox").checked = true;
+        checkBox.checked = true;
     }
 
     if (localStorageSetting === "true") {
         document.body.classList.add("darkmode");
-        document.getElementById("darkmode-checkbox").checked = true;
+        checkBox.checked = true;
     }
+
+    //add transition to darkmode
+    Array.from(document.getElementsByClassName("darkmode-slider")).forEach((element) => element.classList.add("transition"));
 }
 window.onload = setDarkModePreference;
