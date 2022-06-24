ddc8a3ccd59458796a2f97803492a1f543eed719
Julian <julianjesacher@gmail.com>
2022-06-24 23:07:54 +0200

    Implement transitioning lottiefiles

---
+++ darkmode.controller.js
@@ -0,0 +1,42 @@
+//Darkmode
+document.getElementById("darkmode-checkbox").addEventListener("click", (event) => {
+    document.body.classList.toggle("darkmode");
+    localStorage.setItem("darkmodeActive", event.target.checked);
+});
+
+window.addEventListener("load", function (event) {
+    const localStorageSetting = localStorage.getItem("darkmodeActive");
+    const checkBox = document.getElementById("darkmode-checkbox");
+
+    if (!localStorageSetting && window.matchMedia("(prefers-color-scheme:dark)").matches) {
+        document.body.classList.add("darkmode");
+        checkBox.checked = true;
+    }
+
+    if (localStorageSetting === "true") {
+        document.body.classList.add("darkmode");
+        checkBox.checked = true;
+    }
+
+    Array.from(document.getElementsByClassName("social-icon")).forEach((socialIcon) => {
+        const linkElem = document.createElement("link");
+        linkElem.setAttribute("rel", "styles/welcome-section.styles.css");
+        socialIcon.shadowRoot.appendChild(linkElem);
+
+        paths = socialIcon.shadowRoot.querySelectorAll("svg path");
+        paths.forEach((path) => {
+            path.classList.add("social-icon");
+            path.removeAttribute("fill");
+        });
+    });
+});
+
+setTimeout(() => {
+    Array.from(document.getElementsByClassName("social-icon")).forEach((socialIcon) => {
+        paths = socialIcon.shadowRoot.querySelectorAll("svg path");
+        paths.forEach((path) => {
+            path.classList.add("social-icon");
+            path.removeAttribute("fill");
+        });
+    });
+}, 100);
