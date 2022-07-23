fc14cf75febd42cf516bbd0afd715b6b784a0695
Julian <julianjesacher@gmail.com>
2022-07-23 21:33:16 +0200

    Remove redundant code

--- footer.controller.js
+++ footer.controller.js
@@ -2,7 +2,6 @@
   Detect window resizing and if value exceeded, change footer-background text content
 */
 const handleFooterBackgroundText = () => {
-    console.log("HEY")
     textElements = Array.from(document.getElementsByClassName("footer-background-text"));
     if (window.innerWidth > 1250) {
         textElements.forEach((textElement) => (textElement.textContent = "JULIAN JESACHER"));
