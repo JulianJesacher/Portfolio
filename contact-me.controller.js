c64b644460d0bbe88686a00cbade2bc7c4fe6646
Julian <julianjesacher@gmail.com>
2022-07-27 21:12:54 +0200

    Make contact-me-page responsive on desktop view and improve html

--- contact-me.controller.js
+++ contact-me.controller.js
@@ -1,3 +1,16 @@
 window.captcha_successful = () => {
     document.getElementById("sendEmailButton").removeAttribute("disabled");
 };
+
+const handleDifferentContactPages = () => {
+    contactPage = document.getElementsByClassName("contact-me-page")[0];
+    if (window.innerWidth > 1000) {
+        contactPage.classList.remove('mobile');
+        contactPage.classList.add('desktop');
+    } else {
+        contactPage.classList.remove('desktop');
+        contactPage.classList.add('mobile');
+    }
+};
+window.addEventListener("resize", handleDifferentContactPages);
+window.addEventListener('load', handleDifferentContactPages);
