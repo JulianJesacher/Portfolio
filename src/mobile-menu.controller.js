9e9a9d476e5061d85bd36bcc21f2b62c2d2dff3a
Julian <julianjesacher@gmail.com>
2022-07-29 23:20:04 +0200

    Add functionality to mobile-menu

--- src/mobile-menu.controller.js
+++ src/mobile-menu.controller.js
@@ -1,12 +1,16 @@
 const openMobileMenu = () => {
-    const menu = document.getElementById('mobile-menu');
-    menu.style.display = 'grid';
+    const menu = document.getElementById("mobile-menu");
+    menu.style.display = "grid";
+
+    //Add eventlistener to close menu if option is selected
+    const mobileMenuItems = Array.from(document.querySelectorAll(".mobile-menu li a"));
+    mobileMenuItems.forEach((menuItem) => menuItem.addEventListener("click", closeMobileMenu));
 };
 
 const closeMobileMenu = () => {
-    const menu = document.getElementById('mobile-menu');
-    menu.style.display = 'none';
-}
+    const menu = document.getElementById("mobile-menu");
+    menu.style.display = "none";
+};
 
-document.getElementById('open-mobile-menu').addEventListener('click', openMobileMenu);
-document.getElementById('close-mobile-menu').addEventListener('click', closeMobileMenu);
+document.getElementById("open-mobile-menu").addEventListener("click", openMobileMenu);
+document.getElementById("close-mobile-menu").addEventListener("click", closeMobileMenu);
