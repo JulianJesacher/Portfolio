34a30eeec54ba23aff8e5b1ab7aa17c17c04b106
Julian <julianjesacher@gmail.com>
2022-07-30 21:09:45 +0200

    Add closing functionality to contact-form

--- src/contact-me.controller.js
+++ src/contact-me.controller.js
@@ -5,15 +5,58 @@ window.captcha_expired = () => {
     document.getElementById("sendEmailButton").addAttribute("disabled", "disabled");
 };
 
-const handleDifferentContactPages = () => {
+const setCorrectForm = () => {
     const contactPage = document.getElementById("contact-me-page");
     if (window.innerWidth > 1000) {
-        contactPage.classList.remove('mobile');
-        contactPage.classList.add('desktop');
+        contactPage.classList.remove("mobile");
+        contactPage.classList.add("desktop");
     } else {
-        contactPage.classList.remove('desktop');
-        contactPage.classList.add('mobile');
+        contactPage.classList.remove("desktop");
+        contactPage.classList.add("mobile");
     }
 };
-window.addEventListener("resize", handleDifferentContactPages);
-window.addEventListener('load', handleDifferentContactPages);
+
+window.addEventListener('resize', setCorrectForm);
+
+const stopPropagation = (event) => {
+    event.stopPropagation();
+};
+
+const addPathEventListeners = () => {
+    const contactFormPaths = Array.from(document.querySelectorAll(".contact-me-wrapper svg g"));
+    contactFormPaths.forEach((path) => path.addEventListener("click", stopPropagation));
+};
+
+const removePathEventListeners = () => {
+    const contactFormPaths = Array.from(document.querySelectorAll(".contact-me-wrapper svg g"));
+    contactFormPaths.forEach((path) => path.removeEventListener("click", stopPropagation));
+};
+
+const openForm = () => {
+    const contactPage = document.getElementById("contact-me-page");
+    contactPage.style.display = "grid";
+    document.body.style.overflow = "hidden";
+    addPathEventListeners();
+
+    //Timeout to not trigger event when contact-me button is clicked
+    setTimeout(() => {
+        const closingListenerFunction = (event) => {
+            closeForm();
+            document.removeEventListener("click", closingListenerFunction);
+            removePathEventListeners()
+        };
+
+        document.addEventListener("click", closingListenerFunction);
+    }, 0);
+};
+
+const closeForm = () => {
+    const contactPage = document.getElementById("contact-me-page");
+    contactPage.style.display = "none";
+    document.body.style.overflow = "visible";
+};
+
+const contactButtons = Array.from(document.getElementsByClassName("contact-btn"));
+contactButtons.forEach((button) => {
+    button.addEventListener("click", openForm);
+});
