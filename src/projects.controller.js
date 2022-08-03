4cf070da7294ab810b5184d2c65e8dc7bb2b9541
Julian <julianjesacher@gmail.com>
2022-08-03 17:54:04 +0200

    Implement basic foundation of vertical scroll

--- src/projects.controller.js
+++ src/projects.controller.js
@@ -25,8 +25,12 @@ const getJsonData = async (path) => {
 };
 
 const getProject = (projectData) => {
-    //Project container
+    //Project-wrapper
+    const projectWrapper = document.createElement("div");
+    projectWrapper.classList.add("project-wrapper");
+    //Project
     const projectElement = document.createElement("div");
+    projectWrapper.appendChild(projectElement);
     projectElement.classList.add("project");
     if (!projectData["demo-link"]) {
         projectElement.classList.add("no-demo");
@@ -97,13 +101,49 @@ const getProject = (projectData) => {
     }
 
     projectElement.appendChild(projectContent);
-    return projectElement;
+    return projectWrapper;
 };
 
 const showProjects = async () => {
     const projectsData = await getJsonData("./projects.json");
-    const projectsContainer = document.getElementById("projects-wrapper");
+    const projectsContainer = document.getElementById("projects-container");
     projectsData.forEach((singleProjectData) => projectsContainer.appendChild(getProject(singleProjectData)));
+    projectsContainer.style.width = projectsData.length * 100 + "%";
 };
 
 window.addEventListener("load", showProjects);
+
+const projectContainer = document.getElementById("projects-container");
+
+/*
+const projects = gsap.utils.toArray("#projects-container .project-wrapper");
+tween = gsap.to(projects, {
+    xPercent: -100 * (projects.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#projects-container",
+        pin: true,
+        start: "top top",
+        scrub: 1,
+        snap: {
+            snapTo: 1 / (projects.length - 1),
+            inertia: false,
+            duration: { min: 0.1, max: 0.1 },
+        },
+        end: () => "+=" + (projectContainer.offsetWidth - innerWidth),
+    },
+});
+*/
+
+gsap.to(projects, {
+    xPercent: -100 * (projects.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#projects-container",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (projects.length - 1),
+      end: "+=3500",
+    }
+  });
+  
